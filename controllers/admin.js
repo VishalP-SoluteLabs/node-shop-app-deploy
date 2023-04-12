f8c5cf217d962bd452bb27ec5576bf73eb002b4f
Vishal Pansuriya <19it.vishal.pansuriya@gmail.com>
2023-04-12 14:09:13 +0530

    sdsss

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,7 +2,7 @@ const Product = require('../models/product.js');
 const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 const { validationResult } = require('express-validator');
-require('dotenv').config();
+// require('dotenv').config();
 
 const fileHelper = require('../util/file.js');
 
